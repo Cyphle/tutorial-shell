b7f1d4a0153e64f3d8fcf80c3c0d27437a76977a
Cyphle <cyril.phamle@gmail.com>
2023-01-07 20:03:43 +0100

    more scripts

--- cd-history/cd-history.sh
+++ cd-history/cd-history.sh
@@ -4,17 +4,25 @@ pushd() {
 	dirname=$1
 
 	# Replace this by writing to history file
+	if [ -n $direname ] && [ \( -d "$dirname" \) -a \( -x "$dirname" \) ]; then
 	# :- if varname exists and isn't null return value other return right
 	# ${ } is command substitution
-	DIR_STACK="$dirname ${DIR_STACK:-PWD' '}"
-	# :? use varname and if not exist or null raise error
-	cd ${dirname:?"missing directory name."}
-	echo "$DIR_STACK"
+	  DIR_STACK="$dirname ${DIR_STACK:-PWD' '}"
+	  # :? use varname and if not exist or null raise error
+	  cd ${dirname:?"missing directory name."}
+	  echo "$DIR_STACK"
+	else
+	  echo "still in $PWD."
+	fi
 }
 
 popd() {
-	# #* remove pattern match and return rest (it is pattern matching)
-	DIR_STACK=${DIR_STACK#* }
-	cd ${DIR_STACK%% *}
-	echo "$PWD"
+  if [ -n "$DIR_STACK" ]; then
+	  # #* remove pattern match and return rest (it is pattern matching)
+	  DIR_STACK=${DIR_STACK#* }
+	  cd ${DIR_STACK%% *}
+	  echo "$PWD"
+	else
+	  echo "stack empty, still in $PWD"
+	fi
 }
